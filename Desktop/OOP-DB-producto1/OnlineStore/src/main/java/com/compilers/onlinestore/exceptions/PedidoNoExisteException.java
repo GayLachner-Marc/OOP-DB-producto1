ac066ccf8d58f87e89212f2de255109a8c6286d4
Emanuel Rodriguez <121460531+DevEmanuelRodriguez@users.noreply.github.com>
2026-03-11 11:18:41 +0100

    Delete Desktop/OOP-DB-producto1 directory

--- Desktop/OOP-DB-producto1/OnlineStore/src/main/java/com/compilers/onlinestore/exceptions/PedidoNoExisteException.java
+++
@@ -1,7 +0,0 @@
-package com.compilers.onlinestore.exceptions;
-
-public class PedidoNoExisteException extends Exception {
-    public PedidoNoExisteException(String mensaje) {
-        super(mensaje);
-    }
-}
