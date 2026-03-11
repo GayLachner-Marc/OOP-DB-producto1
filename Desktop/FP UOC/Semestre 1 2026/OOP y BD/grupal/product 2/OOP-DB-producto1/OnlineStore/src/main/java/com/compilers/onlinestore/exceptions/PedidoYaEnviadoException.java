33eb7a3fbd9769e1976edcbe21019fe57b3f4976
Emanuel Rodriguez <121460531+DevEmanuelRodriguez@users.noreply.github.com>
2026-03-11 11:18:31 +0100

    Delete Desktop/FP UOC/Semestre 1 2026/OOP y BD/grupal/product 2/OOP-DB-producto1 directory

--- Desktop/FP UOC/Semestre 1 2026/OOP y BD/grupal/product 2/OOP-DB-producto1/OnlineStore/src/main/java/com/compilers/onlinestore/exceptions/PedidoYaEnviadoException.java
+++
@@ -1,9 +0,0 @@
-
-package com.compilers.onlinestore.exceptions;
-
-
-public class PedidoYaEnviadoException extends Exception {
-    public PedidoYaEnviadoException(String mensaje) {
-        super(mensaje);
-    }
-}
