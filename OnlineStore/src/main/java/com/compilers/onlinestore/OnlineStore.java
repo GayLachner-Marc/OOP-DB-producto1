0e73ebe4e749222b3f292c9b5f678ded2a27913e
GayLachner-Marc <mgayl@uoc.edu>
2026-03-23 17:44:29 +0100

    Cambios en OnlineStore.Java

--- OnlineStore/src/main/java/com/compilers/onlinestore/OnlineStore.java
+++ OnlineStore/src/main/java/com/compilers/onlinestore/OnlineStore.java
@@ -1,51 +1,17 @@
 package com.compilers.onlinestore;
 
-
-import com.compilers.onlinestore.model.Tienda;
-import com.compilers.onlinestore.model.Articulos.Articulo;
-import com.compilers.onlinestore.model.Clientes.Cliente;
-import com.compilers.onlinestore.model.Clientes.ClienteEstandar;
-import com.compilers.onlinestore.model.Clientes.ClientePremium;
-import com.compilers.onlinestore.model.Pedidos.Pedido;
+import com.compilers.onlinestore.controller.Controladora;
+import com.compilers.onlinestore.exceptions.ArticuloNoExisteException;
+import com.compilers.onlinestore.exceptions.PedidoYaEnviadoException;
+import com.compilers.onlinestore.exceptions.ClienteNoExisteException;
+import com.compilers.onlinestore.exceptions.PedidoNoExisteException;
+import com.compilers.onlinestore.view.ConsolaView;
 
 public class OnlineStore {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClienteNoExisteException, PedidoNoExisteException, ArticuloNoExisteException, PedidoYaEnviadoException {
+         Controladora controladora = new Controladora();
+        ConsolaView vista = new ConsolaView(controladora);
 
-        //TEST
-        // Crear tienda
-        Tienda tienda = new Tienda("Online Store");
-
-        // Crear controlador lógico
-        ControladoraLogicaTiendaOnline controladora = new ControladoraLogicaTiendaOnline(tienda);
-
-        // Crear clientes
-        Cliente c1 = new ClienteEstandar("Juan", "Calle 1", "123A", "juan@email.com");
-        Cliente c2 = new ClientePremium("Ana", "Calle 2", "456B", "ana@email.com");
-
-        tienda.añadirCliente(c1);
-        tienda.añadirCliente(c2);
-
-        // Crear artículos (String codigo, String descripcion, double precioVenta,double gastosEnvio, int tiempoPreparacion) 
-        Articulo a1 = new Articulo("A1", "Portátil", 1000, 20, 60);
-        tienda.añadirArticulo(a1);
-
-        // Crear pedido (int numeroPedido, Cliente cliente, Articulo articulo, int cantidad)
-        Pedido p1 = new Pedido(1, c2, a1, 2);
-        tienda.añadirPedido(p1);
-
-        // Probar cálculo total
-        System.out.println("Total pedido: " + p1.calcularTotal());
-
-        // Probar estado envío
-        System.out.println("Esta enviado? " + p1.estaEnviado());
-
-        // Mostrar pedidos pendientes
-        System.out.println("Pedidos pendientes:");
-        for (Pedido p : tienda.mostrarPedidosPendientes()) {
-            System.out.println("Pedido numero " + p);
-        }
-    }
-
+        vista.iniciar(); }
 }
-
