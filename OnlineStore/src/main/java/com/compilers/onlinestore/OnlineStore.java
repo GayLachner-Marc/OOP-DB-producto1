5690b6cb25eab251b4df3df80fd3e640bec9b357
GayLachner-Marc <mgayl@uoc.edu>
2026-05-08 20:17:58 +0200

    Instalacion de las dependencies y plugins para trabajar en JavaFX y una ligera modificación de la clase OnlineStore para empezar a funcionar con JavaFX

--- OnlineStore/src/main/java/com/compilers/onlinestore/OnlineStore.java
+++ OnlineStore/src/main/java/com/compilers/onlinestore/OnlineStore.java
@@ -7,11 +7,31 @@ import com.compilers.onlinestore.exceptions.ClienteNoExisteException;
 import com.compilers.onlinestore.exceptions.PedidoNoExisteException;
 import com.compilers.onlinestore.view.ConsolaView;
 
-public class OnlineStore {
 
-    public static void main(String[] args) throws ClienteNoExisteException, PedidoNoExisteException, ArticuloNoExisteException, PedidoYaEnviadoException {
-         Controladora controladora = new Controladora();
-        ConsolaView vista = new ConsolaView(controladora);
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-        vista.iniciar(); }
+public class OnlineStore extends Application {
+ @Override
+    public void start(Stage stage) {
+
+        VBox root = new VBox();
+
+        root.getChildren().add(
+            new Label("OnlineStore funcionando con JavaFX 🚀")
+        );
+
+        Scene scene = new Scene(root, 500, 300);
+
+        stage.setTitle("OnlineStore");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
