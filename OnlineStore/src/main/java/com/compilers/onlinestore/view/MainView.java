055d31d65ce7c7b4bb695543386eac5d1e3a884d
EmanuelRodriguez <jrodriguezapa@uoc.edu>
2026-05-10 03:55:41 +0200

    Configure JavaFX Maven setup

---
+++ OnlineStore/src/main/java/com/compilers/onlinestore/view/MainView.java
@@ -0,0 +1,26 @@
+package com.compilers.onlinestore.view;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainView extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        Label label = new Label("JavaFX funciona correctamente");
+
+        StackPane root = new StackPane(label);
+        Scene scene = new Scene(root, 500, 300);
+
+        stage.setTitle("Online Store");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
