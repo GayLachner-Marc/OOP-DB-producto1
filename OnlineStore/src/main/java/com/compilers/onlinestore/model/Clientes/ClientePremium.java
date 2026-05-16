44100077b068e069136030026881d33ffc0d35ec
EmanuelRodriguez <jrodriguezapa@uoc.edu>
2026-05-16 12:26:21 +0200

    Ya funciona el CRUD cliente, se debe ajusta vistas

--- OnlineStore/src/main/java/com/compilers/onlinestore/model/Clientes/ClientePremium.java
+++ OnlineStore/src/main/java/com/compilers/onlinestore/model/Clientes/ClientePremium.java
@@ -1,65 +1,65 @@
 package com.compilers.onlinestore.model.Clientes;
+
 import jakarta.persistence.*;
+
 @Entity
 @DiscriminatorValue("PREMIUM")
+public class ClientePremium extends Cliente {
 
-public class ClientePremium extends Cliente{
-
-    private double cuotaAnual = 30.0;
+    @Column(name = "cuota_anual")
+private double cuotaAnual = 30.0;
 
     public ClientePremium() {
         super();
     }
-    
-    public ClientePremium(String nombre, String domicilio, String nif, String email) {
-        super(nombre, domicilio, nif, email);
+
+    public ClientePremium(
+            String nombre,
+            String domicilio,
+            String nif,
+            String email
+    ) {
+        super(
+                nombre,
+                domicilio,
+                nif,
+                email
+        );
+    }
+
+    public ClientePremium(
+            String nombre,
+            String domicilio,
+            String nif,
+            String email,
+            double cuotaAnual
+    ) {
+
+        super(
+                nombre,
+                domicilio,
+                nif,
+                email
+        );
+
+        this.cuotaAnual = cuotaAnual;
     }
 
     public double getCuotaAnual() {
         return cuotaAnual;
     }
 
-    public String getNombre() {
-        return nombre;
+    public void setCuotaAnual(double cuotaAnual) {
+        this.cuotaAnual = cuotaAnual;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getDomicilio() {
-        return domicilio;
-    }
-
-    public void setDomicilio(String domicilio) {
-        this.domicilio = domicilio;
-    }
-
-    public String getNif() {
-        return nif;
-    }
-
-    public void setNif(String nif) {
-        this.nif = nif;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    @Override
+    public double calcularDescuentoEnvio() {
+        return 0.20;
     }
 
     @Override
     public String toString() {
-        return "ClientePremium{" + "cuotaAnual=" + cuotaAnual + '}';
-    }
-    
-    
-
-    @Override
-    public double calcularDescuentoEnvio() {
-        return 0.20; // 20%
+        return nombre + " - " + email;
     }
 }
--- OnlineStore/src/main/java/com/compilers/onlinestore/view/controllers/ClientesController.java
+++ OnlineStore/src/main/java/com/compilers/onlinestore/view/controllers/ClientesController.java
@@ -7,13 +7,17 @@ import com.compilers.onlinestore.model.Clientes.ClientePremium;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import com.compilers.onlinestore.model.Clientes.ClienteEstandar;
 
 public class ClientesController {
 
@@ -37,9 +41,40 @@ public class ClientesController {
 
     @FXML
     private TableColumn<Cliente, Double> colCuotaAnual;
+    
+    @FXML
+private TableColumn<Cliente, Void> colAcciones;
+
+@FXML
+private VBox panelNuevoCliente;
+
+@FXML
+private VBox cardTablaClientes;
+
+@FXML
+private TextField txtNombre;
+
+@FXML
+private TextField txtEmail;
+
+@FXML
+private TextField txtDomicilio;
+
+@FXML
+private TextField txtNif;
+
+@FXML
+private ComboBox<String> comboTipoCliente;
+
+@FXML
+private TextField txtCuotaAnual;
 
     private final Controladora controladora =
             new Controladora();
+    
+    private boolean modoEdicion = false;
+
+private Cliente clienteEditando = null;
 
     @FXML
     public void initialize() {
@@ -98,9 +133,183 @@ public class ClientesController {
                     cuota
             );
         });
+        
+        //añadido
+        comboTipoCliente.getItems().addAll(
+        "ESTANDAR",
+        "PREMIUM"
+);
+
+comboTipoCliente.setOnAction(event -> {
+
+    boolean premium =
+            "PREMIUM".equals(
+                    comboTipoCliente.getValue()
+            );
+
+    txtCuotaAnual.setVisible(
+            premium
+    );
+
+    txtCuotaAnual.setManaged(
+            premium
+    );
+
+    if (!premium) {
+        txtCuotaAnual.clear();
+    }
+});
+
+configurarColumnaAcciones();
+        
+        
 
         cargarClientes();
     }
+    
+    private void configurarColumnaAcciones() {
+
+    colAcciones.setCellFactory(param ->
+            new TableCell<Cliente, Void>() {
+
+        private final Button btnEditar =
+                new Button("✏");
+
+        private final Button btnEliminar =
+                new Button("🗑");
+
+        private final HBox botones =
+                new HBox(
+                        10,
+                        btnEditar,
+                        btnEliminar
+                );
+
+        {
+
+            botones.setStyle(
+                    "-fx-alignment: center;"
+            );
+
+            btnEditar.setStyle("""
+                -fx-background-color: transparent;
+                -fx-font-size: 16px;
+                -fx-cursor: hand;
+            """);
+
+            btnEliminar.setStyle("""
+                -fx-background-color: transparent;
+                -fx-font-size: 16px;
+                -fx-cursor: hand;
+            """);
+
+            btnEditar.setOnAction(event -> {
+
+                Cliente cliente =
+                        getTableView()
+                                .getItems()
+                                .get(getIndex());
+
+                modoEdicion = true;
+                clienteEditando = cliente;
+
+                panelNuevoCliente.setVisible(true);
+                panelNuevoCliente.setManaged(true);
+
+                cardTablaClientes.setVisible(false);
+                cardTablaClientes.setManaged(false);
+
+                txtNombre.setText(
+                        cliente.getNombre()
+                );
+
+                txtEmail.setText(
+                        cliente.getEmail()
+                );
+
+                txtDomicilio.setText(
+                        cliente.getDomicilio()
+                );
+
+                txtNif.setText(
+                        cliente.getNif()
+                );
+
+                txtEmail.setDisable(true);
+
+                if (cliente instanceof ClientePremium premium) {
+
+                    comboTipoCliente.setValue(
+                            "PREMIUM"
+                    );
+
+                    txtCuotaAnual.setVisible(true);
+                    txtCuotaAnual.setManaged(true);
+
+                    txtCuotaAnual.setText(
+                            String.valueOf(
+                                    premium.getCuotaAnual()
+                            )
+                    );
+
+                } else {
+
+                    comboTipoCliente.setValue(
+                            "ESTANDAR"
+                    );
+
+                    txtCuotaAnual.clear();
+                    txtCuotaAnual.setVisible(false);
+                    txtCuotaAnual.setManaged(false);
+                }
+            });
+
+            btnEliminar.setOnAction(event -> {
+
+                Cliente cliente =
+                        getTableView()
+                                .getItems()
+                                .get(getIndex());
+
+                try {
+
+                    controladora.eliminarCliente(
+                            cliente.getEmail()
+                    );
+
+                    cargarClientes();
+
+                } catch (Exception e) {
+
+                    System.out.println(
+                            e.getMessage()
+                    );
+                }
+            });
+        }
+
+        @Override
+        protected void updateItem(
+                Void item,
+                boolean empty
+        ) {
+
+            super.updateItem(
+                    item,
+                    empty
+            );
+
+            if (empty) {
+
+                setGraphic(null);
+
+            } else {
+
+                setGraphic(botones);
+            }
+        }
+    });
+}
 
     private void cargarClientes() {
 
@@ -111,89 +320,161 @@ public class ClientesController {
 
         tablaClientes.setItems(lista);
     }
-
+    
+    
     @FXML
-    private void abrirFormularioCliente() {
+private void abrirFormularioCliente() {
 
-        try {
+    modoEdicion = false;
+    clienteEditando = null;
 
-            FXMLLoader loader =
-                    new FXMLLoader(
-                            getClass().getResource(
-                                    "/fxml/nuevo_cliente.fxml"
-                            )
-                    );
+    panelNuevoCliente.setVisible(true);
+    panelNuevoCliente.setManaged(true);
 
-            Parent root = loader.load();
+    cardTablaClientes.setVisible(false);
+    cardTablaClientes.setManaged(false);
 
-            Stage stage = new Stage();
+    txtNombre.clear();
+    txtEmail.clear();
+    txtDomicilio.clear();
+    txtNif.clear();
+    txtCuotaAnual.clear();
 
-            stage.setTitle(
-                    "Nuevo Cliente"
+    txtEmail.setDisable(false);
+
+    comboTipoCliente
+            .getSelectionModel()
+            .clearSelection();
+}
+
+@FXML
+private void cancelarNuevoCliente() {
+
+    modoEdicion = false;
+    clienteEditando = null;
+
+    panelNuevoCliente.setVisible(false);
+    panelNuevoCliente.setManaged(false);
+
+    cardTablaClientes.setVisible(true);
+    cardTablaClientes.setManaged(true);
+
+    txtEmail.setDisable(false);
+}
+
+@FXML
+private void guardarCliente() {
+
+    try {
+
+        String nombre =
+                txtNombre.getText().trim();
+
+        String email =
+                txtEmail.getText().trim();
+
+        String domicilio =
+                txtDomicilio.getText().trim();
+
+        String nif =
+                txtNif.getText().trim();
+
+        String tipo =
+                comboTipoCliente.getValue();
+
+        if (nombre.isBlank()
+                || email.isBlank()
+                || domicilio.isBlank()
+                || nif.isBlank()
+                || tipo == null) {
+
+            System.out.println(
+                    "Todos los campos son obligatorios"
             );
 
-            stage.initModality(
-                    Modality.APPLICATION_MODAL
-            );
-
-            stage.setScene(
-                    new Scene(root)
-            );
-
-            stage.showAndWait();
-
-            cargarClientes();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @FXML
-    private void editarCliente() {
-
-        Cliente clienteSeleccionado =
-                tablaClientes
-                        .getSelectionModel()
-                        .getSelectedItem();
-
-        if (clienteSeleccionado == null) {
             return;
         }
 
-        try {
+        if (modoEdicion) {
 
-            FXMLLoader loader =
-                    new FXMLLoader(
-                            getClass().getResource(
-                                    "/fxml/editar_cliente.fxml"
-                            )
-                    );
-
-            Parent root =
-                    loader.load();
-
-            Stage stage =
-                    new Stage();
-
-            stage.setTitle(
-                    "Editar Cliente"
+            clienteEditando.setNombre(
+                    nombre
             );
 
-            stage.initModality(
-                    Modality.APPLICATION_MODAL
+            clienteEditando.setDomicilio(
+                    domicilio
             );
 
-            stage.setScene(
-                    new Scene(root)
+            clienteEditando.setNif(
+                    nif
             );
 
-            stage.showAndWait();
+            if (clienteEditando
+                    instanceof ClientePremium premium) {
 
-            cargarClientes();
+                premium.setCuotaAnual(
+                        Double.parseDouble(
+                                txtCuotaAnual
+                                        .getText()
+                                        .trim()
+                        )
+                );
+            }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+            controladora.actualizarCliente(
+                    clienteEditando
+            );
+
+        } else {
+
+            Cliente cliente;
+
+            if ("PREMIUM".equals(tipo)) {
+
+                double cuota =
+                        Double.parseDouble(
+                                txtCuotaAnual
+                                        .getText()
+                                        .trim()
+                        );
+
+                cliente =
+                        new ClientePremium(
+                                nombre,
+                                domicilio,
+                                nif,
+                                email,
+                                cuota
+                        );
+
+            } else {
+
+                cliente =
+                        new ClienteEstandar(
+                                nombre,
+                                domicilio,
+                                nif,
+                                email
+                        );
+            }
+
+            controladora.crearCliente(
+                    cliente
+            );
         }
+
+        cargarClientes();
+
+        cancelarNuevoCliente();
+
+    } catch (Exception e) {
+
+        e.printStackTrace();
     }
+}
+
+
+
+
+    
 }
